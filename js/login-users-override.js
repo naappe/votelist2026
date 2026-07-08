40666d2b6cdb99600b60972412eac1272dc2c990
naappe <naappe@gmail.com>
2026-07-08 20:31:00 +0500

    Add login username mappings

---
+++ js/login-users-override.js
@@ -0,0 +1,8 @@
+(function(){
+  window.APP_CONFIG = window.APP_CONFIG || {};
+  window.APP_CONFIG.loginUsers = {
+    admin: { username: 'admin', email: 'naappe@gmail.com', role: 'admin', party: 'ALL' },
+    pnc: { username: 'pnc2026', email: 'pnc2026@gmail.com', role: 'member', party: 'PNC' },
+    mdp: { username: 'mdp2026', email: 'whitesafrron2025@gmail.com', role: 'member', party: 'MDP' }
+  };
+})();
